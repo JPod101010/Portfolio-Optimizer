f8c25f8aa65f894b2c63845a3c928caa3ace44ee
JPod101010 <jarkopodmajersky013@gmail.com>
2025-11-18 23:06:48 +0100

    feat(TSA): basic ts analysis

--- src/TimeSeriesAnalyser.py
+++ src/TimeSeriesAnalyser.py
@@ -1,5 +1,6 @@
 import numpy as np
-from math import sqrt
+from math import sqrt, prod
+from typing import Tuple, List
 
 time_series_t = np.ndarray[1,float]
 
@@ -70,3 +71,75 @@ class TimeSeriesAnalyser(Filters):
         ) / TimeSeriesAnalyser.Mean(
             input_array
         ))
+    
+    @staticmethod
+    def GeometricMean(input_array : time_series_t) -> float:
+        return prod(input_array) ** (1/len(input_array))
+
+
+    @staticmethod
+    def Covariance(input_arrayA : time_series_t, 
+                   input_arrayB : time_series_t) -> float:
+        """
+        Tells how are the two time series correlated:
+        >0 : positive correlation
+        0  : no correlation
+        <0 : negative correlation 
+        """
+        return TimeSeriesAnalyser.Mean(
+            (input_arrayA - TimeSeriesAnalyser.Mean(input_arrayA)) * 
+            (input_arrayB - TimeSeriesAnalyser.Mean(input_arrayB))
+        )
+
+
+    @staticmethod
+    def Correlation(input_arrayA : time_series_t, 
+                    input_arrayB : time_series_t) -> float:
+        """
+        Pearsons correlation coefficient TODO:
+        """
+        MEAN_A = TimeSeriesAnalyser.Mean(input_arrayA)
+        MEAN_B = TimeSeriesAnalyser.Mean(input_arrayB)
+
+        numerator = sum((input_arrayA - MEAN_A) * (input_arrayB - MEAN_B))
+        denominator = sqrt(
+            sum((input_arrayA - MEAN_A**2)) *
+            sum((input_arrayB - MEAN_B**2))
+        )
+
+        return numerator / denominator
+
+    @staticmethod
+    def AutoCorrelation(input_array : time_series_t, lag : int = 1) -> float:
+        return TimeSeriesAnalyser.Correlation(
+            input_arrayA=input_array[:-lag],
+            input_arrayB=input_array[lag:]
+        )
+
+    
+    @staticmethod
+    def AutoCorrelationAnalysis(input_array : time_series_t, MAX_LAG = 30) -> List[float]:
+        MAX_LAG = min(MAX_LAG, len(input_array) - 1)
+        ret_l = []
+        for lag in range(1, MAX_LAG):
+            ret_l.append(
+                TimeSeriesAnalyser.AutoCorrelation(
+                    input_array, lag
+                )
+            )
+        return ret_l
+
+    
+    @staticmethod
+    def BasicAnalysis(input_array : time_series_t) -> Tuple[float]:
+        EX = TimeSeriesAnalyser.Mean(input_array)
+        VX = TimeSeriesAnalyser.Variance(input_array)
+        DX = TimeSeriesAnalyser.StandardDeviation(input_array)
+        CV = TimeSeriesAnalyser.CoefficientOfVariance(input_array)
+        EDM = EX * CV 
+
+        print(f"Mean : {EX}\nVariance : {VX}\nStandard-Deviation : {DX}\nCoefficient of variance : {CV}\nExpected daily move : {EDM}")
+
+        return (
+            EX, VX, DX, CV
+        )
